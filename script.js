571da29377446d3d31ebf2e69723586be6ae3530
Jonathan Martinez <jonmar.dev@gmail.com>
2025-05-17 02:38:41 -0700

    feat: add close button functionality to hide meme modal

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const getImageBtn = document.getElementById("get-image-btn");
 const gifOnlyOption = document.getElementById("gifs-only-option");
 const memeModalInner = document.getElementById("meme-modal-inner");
 const memeModal = document.getElementById("meme-modal");
+const closeModalBtn = document.getElementById("meme-modal-close-btn");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
 getImageBtn.addEventListener("click", renderCat);
+closeModalBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  memeModal.style.display = "none";
+}
 
 function highlightCheckedOption(e) {
   const radios = document.getElementsByClassName("radio");
